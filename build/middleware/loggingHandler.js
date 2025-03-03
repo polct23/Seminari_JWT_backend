be8b272645f951c1aefe3c076006c59a0908974f
JoanPerezPerez <joan.perez.p@estudiantat.upc.edu>
2025-03-03 15:47:32 +0100

    Eliminar carpeta del repo

--- build/middleware/loggingHandler.js
+++
@@ -1,7 +0,0 @@
-export function loggingHandler(req, res, next) {
-    console.log(`Incoming -Method: [${req.method}] -URL [${req.url}] - IP [${req.socket.remoteAddress}]`);
-    res.on('finish', () => {
-        console.log(`Incoming -Method: [${req.method}] -URL [${req.url}] - IP [${req.socket.remoteAddress}] -STATUS [${req.statusCode}]`);
-    });
-    next();
-}
